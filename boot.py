4f4ac111ee221007dbf0bfd596d490362060ad1e
its-fonsy <ciabadiale@gmail.com>
2023-10-16 21:51:16 +0200

    adjusting the keyboard to my handwiring

---
+++ boot.py
@@ -0,0 +1,13 @@
+#import board
+#
+#from kmk.bootcfg import bootcfg
+#
+#bootcfg(
+#    sense=board.GP14,  # column
+#    source=board.GP15, # row
+#    keyboard=True,
+#    midi=False,
+#    mouse=False,
+#    storage=False,
+#    usb_id=('KMK Keyboards', 'Dactyl Mini 5x6'),
+#)
--- user_keymaps/its-fonsy/main.py
+++ user_keymaps/its-fonsy/main.py
@@ -26,12 +26,12 @@ keyboard.modules.append(split)
 
 keyboard.keymap = [
     [   # 0
-        KC.ESC,  KC.N1,   KC.N2,   KC.N3, KC.N4,   KC.N5,                                         KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0,  KC.GRV,
-        KC.CAPS,  KC.Q,    KC.W,    KC.E,  KC.R,    KC.T,                                          KC.Y,  KC.U,    KC.I,    KC.O,    KC.P, KC.BSLS,
-        KC.LSFT,  KC.A,    KC.S,    KC.D,  KC.F,    KC.G,                                          KC.H,  KC.J,    KC.K,    KC.L, KC.SCLN, KC.RSFT,
-        KC.LCTL,  KC.Z,    KC.X,    KC.C,  KC.V,    KC.B,                                          KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RCTL,
-                        KC.LEFT, KC.RGHT,        KC.Q, KC.SPC,   KC.W,      KC.Q, KC.ENT, KC.D,          KC.UP, KC.DOWN,
-                                                           KC.W, KC.S,      KC.W, KC.S,
+        KC.ESC,  KC.N1,   KC.N2,   KC.N3, KC.N4,   KC.N5,                                 KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0,  KC.GRV,
+        KC.CAPS,  KC.Q,    KC.W,    KC.E,  KC.R,    KC.T,                                  KC.Y,  KC.U,    KC.I,    KC.O,    KC.P, KC.BSLS,
+        KC.LSFT,  KC.A,    KC.S,    KC.D,  KC.F,    KC.G,                                  KC.H,  KC.J,    KC.K,    KC.L, KC.SCLN, KC.RSFT,
+        KC.LCTL,  KC.Z,    KC.X,    KC.C,  KC.V,    KC.B,                                  KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.RCTL,
+                        KC.LEFT, KC.RGHT,           KC.Q, KC.SPC, KC.W,      KC.Q, KC.ENT, KC.D,          KC.UP, KC.DOWN,
+                                                            KC.E, KC.S,      KC.W,   KC.S,
     ],
 ]
 
