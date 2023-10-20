d7493187361e710c9302486835e79b4b5cc96f16
its-fonsy <ciabadiale@gmail.com>
2023-10-20 13:30:16 +0200

    adding other features to the thumbcluster

--- user_keymaps/its-fonsy/main.py
+++ user_keymaps/its-fonsy/main.py
@@ -3,6 +3,7 @@ from kb import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.holdtap import HoldTap
 from kmk.modules.layers import Layers
+from kmk.modules.tapdance import TapDance
 from kmk.modules.split import Split, SplitSide
 from storage import getmount
 
@@ -10,6 +11,7 @@ keyboard = KMKKeyboard()
 
 keyboard.modules.append(Layers())
 keyboard.modules.append(HoldTap())
+keyboard.modules.append(TapDance())
 
 side = SplitSide.RIGHT if str(getmount('/').label)[-1] == 'R' else SplitSide.LEFT
 
@@ -31,16 +33,24 @@ CTL_TAB = KC.HT(KC.TAB,  KC.RCTRL)
 SFT_ENT = KC.HT(KC.ENT,  KC.RSFT)
 GUI_BSP = KC.HT(KC.BSPC, KC.RGUI)
 
+L1_BSLS = KC.LT(1, KC.BSLS) 
+L2_PIPE = KC.LT(2, KC.PIPE) 
+
+# TapDance keys
+LBRC_LCBR = KC.TD(KC.LBRC, KC.LCBR)
+RBRC_RCBR = KC.TD(KC.RBRC, KC.RCBR)
+
+
 XXXXXXX = KC.TRNS
 
 keyboard.keymap = [
     [   # Layer 0
-         KC.GRV, KC.N1,   KC.N2,   KC.N3, KC.N4,   KC.N5,                                 KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0, KC.PGUP,
-        KC.CAPS,  KC.Q,    KC.W,    KC.E,  KC.R,    KC.T,                                  KC.Y,  KC.U,    KC.I,    KC.O,    KC.P, KC.BSLS,
-        KC.LSFT,  KC.A,    KC.S,    KC.D,  KC.F,    KC.G,                                  KC.H,  KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,
-        KC.LALT,  KC.Z,    KC.X,    KC.C,  KC.V,    KC.B,                                  KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.PGDW,
-                        KC.MINS, KC.PLUS,        GUI_DEL, SFT_SPC,KC.MO(1),    KC.MO(2),  SFT_ENT, GUI_BSP,    KC.UNDS,  KC.EQL,
-                                                          CTL_ESC, KC.LBRC,     KC.RBRC,  CTL_TAB,
+         KC.GRV,    KC.N1,   KC.N2,   KC.N3, KC.N4,   KC.N5,                                             KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0, KC.PGUP,
+        KC.CAPS,     KC.Q,    KC.W,    KC.E,  KC.R,    KC.T,                                              KC.Y,  KC.U,    KC.I,    KC.O,    KC.P, KC.BSLS,
+        KC.LSFT,     KC.A,    KC.S,    KC.D,  KC.F,    KC.G,                                              KC.H,  KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,
+        KC.LALT,     KC.Z,    KC.X,    KC.C,  KC.V,    KC.B,                                              KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.PGDW,
+                           KC.MINS, KC.PLUS,        GUI_DEL, SFT_SPC,   L1_BSLS,     L2_PIPE, SFT_ENT, GUI_BSP,        KC.UNDS,  KC.EQL,
+                                                             CTL_ESC, LBRC_LCBR,   RBRC_RCBR, CTL_TAB,
     ],
     [   # Layer 1
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
