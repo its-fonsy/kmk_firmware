3e2baa9de790d25865ebd47bd8772e6cb71b1eaa
its-fonsy <ciabadiale@gmail.com>
2023-10-20 13:29:49 +0200

    adding delays to unplug and replug the kb when flashing the mcu

--- flash.sh
+++ flash.sh
@@ -4,19 +4,40 @@ KB_LABEL="DACTYL"
 USER_KM="user_keymaps/its-fonsy/main.py"
 USER_BOARD="user_keymaps/its-fonsy"
 
-BLOCK_PATH=$(lsblk -l -o PATH,LABEL | grep $KB_LABEL | cut -d" " -f1)
-BLOCK_MOUNTPOINT=$(lsblk -l -o MOUNTPOINT,LABEL | grep $KB_LABEL | cut -d" " -f1)
+# Give the user time to unplug the keyboard
+timeout=3
+echo "Unplug the keyboard"
+while [ $timeout -ne 0 ]
+do
+    echo -ne "\rWaiting $timeout seconds"
+    let timeout--
+    sleep 1
+done
 
-# Check if the USB stick is plugged in
-if [[ -z "$BLOCK_PATH" ]]; then
-    echo "ERROR: can't find the MCU. Is it plugged in correctly?"
-    exit 1
-fi
+# Wait for the KB to be plugged in
+echo -ne "\nPlug the keyboard"
+timeout=20
+while [ -z "$BLOCK_PATH" ]
+do
+    # MCU isn't plugged in
+    if [[ $timeout -eq 0 ]]; then
+        echo "ERROR: can't find the MCU. Is it plugged in correctly?"
+        exit 1
+    fi
+
+    BLOCK_PATH=$(lsblk -l -o PATH,LABEL | grep $KB_LABEL | cut -d" " -f1)
+    let timeout--
+    sleep 1
+    echo -ne '.'
+done
+
+echo KEYBOARD DETECTED!
 
 # For split keyboard distinguish left and right side
-KB_LABEL=$(lsblk -l -o LABEL | grep $KB_LABEL | cut -d" " -f1)
+#KB_LABEL=$(lsblk -l -o LABEL | grep $KB_LABEL | cut -d" " -f1)
 
 # If the drive isn't already mounted, mount it
+BLOCK_MOUNTPOINT=$(lsblk -l -o MOUNTPOINT,LABEL | grep $KB_LABEL | cut -d" " -f1)
 if [[ -z "$BLOCK_MOUNTPOINT" ]]; then
     udisksctl mount -b $BLOCK_PATH
     BLOCK_MOUNTPOINT=$(lsblk -l -o MOUNTPOINT,LABEL | grep $KB_LABEL | cut -d" " -f1)
