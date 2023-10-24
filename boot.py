93710eafb89e3a6032838416cf29c63d25a0c8f8
its-fonsy <ciabadiale@gmail.com>
2023-10-24 16:48:26 +0200

    show as USB drive only when pressing the "5" or the "PGUP" keys

--- boot.py
+++ boot.py
@@ -1,13 +1,13 @@
-#import board
-#
-#from kmk.bootcfg import bootcfg
-#
-#bootcfg(
-#    sense=board.GP14,  # column
-#    source=board.GP15, # row
-#    keyboard=True,
-#    midi=False,
-#    mouse=False,
-#    storage=False,
-#    usb_id=('KMK Keyboards', 'Dactyl Mini 5x6'),
-#)
+import board
+
+from kmk.bootcfg import bootcfg
+
+bootcfg(
+    sense=board.GP13,  # column
+    source=board.GP2,  # row
+    keyboard=True,
+    midi=False,
+    mouse=False,
+    storage=False,
+    usb_id=('KMK Keyboards', 'Dactyl Mini 5x6'),
+)
