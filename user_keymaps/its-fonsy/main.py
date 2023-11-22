dbe7ae1592ab72ea055e9697a77c02536c8f0be6
its-fonsy <ciabadiale@gmail.com>
2023-11-22 10:15:04 +0100

    fixed PGDOWN keycode wrong spelling

--- user_keymaps/its-fonsy/main.py
+++ user_keymaps/its-fonsy/main.py
@@ -53,7 +53,7 @@ keyboard.keymap = [
         KC.LSFT,    KC.N1,   KC.N2,   KC.N3, KC.N4,   KC.N5,                                             KC.N6, KC.N7,   KC.N8,   KC.N9,   KC.N0, KC.PGUP,
         KC.CAPS,     KC.Q,    KC.W,    KC.E,  KC.R,    KC.T,                                              KC.Y,  KC.U,    KC.I,    KC.O,    KC.P, KC.TILD,
          KC.GRV,     KC.A,    KC.S,    KC.D,  KC.F,    KC.G,                                              KC.H,  KC.J,    KC.K,    KC.L, KC.SCLN, KC.QUOT,
-          KC.CW,     KC.Z,    KC.X,    KC.C,  KC.V,    KC.B,                                              KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.PGDW,
+          KC.CW,     KC.Z,    KC.X,    KC.C,  KC.V,    KC.B,                                              KC.N,  KC.M, KC.COMM,  KC.DOT, KC.SLSH, KC.PGDN,
                            KC.MINS, ALT_PLS,        GUI_DEL, SFT_SPC,   L1_BSLS,     L2_PIPE, SFT_ENT, GUI_BSP,        ALT_UND,  KC.EQL,
                                                              CTL_ESC, LBRC_LCBR,   RBRC_RCBR, CTL_TAB,
     ],
